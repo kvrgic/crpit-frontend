5b68d3caede162ee1e52e27fa87e61971d66fc09
Aida Kvrgić <kvrgicaida95@gmail.com>
2025-06-23 12:46:58 +0200

    Fix url

--- src/redux/features/blogs/blogsApi.js
+++ src/redux/features/blogs/blogsApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const blogApi = createApi({
     reducerPath: 'blogsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://crpit-backend.onrender.com/api/',
+        baseUrl: 'https://crpit-backend.onrender.com/api',
         credentials: 'include',
         prepareHeaders: (headers, { getState }) => {
             const token = getState().auth.user?.token;
